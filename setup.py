2e67b612b8b6bfbda96495b3e19e710c9918cb63
Mondal Rohit (IFMSIN IT RDI ADS WEB) <rohit.mondal@infineon.com>
2024-11-12 15:11:48 +0530

    Add cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='water portability prediction using ML',
+    author='rohit mondal',
+    license='',
+)
